c29fce57de6929cd3cbf1e751b6d50f858dba7d1
Josh Goebel <dreamer3@gmail.com>
2009-04-05 09:37:08 -0400

    refactoring

--- lib/dreamer3/ranged_find.rb
+++ lib/dreamer3/ranged_find.rb
@@ -4,21 +4,9 @@ module Dreamer3 # :nodoc:
    module ClassMethods
 
      def count_with_when(*args)
-       options=begin
-         if args.is_a? Array
-           if args[0].is_a? Hash
-             args[0]
-           elsif args[1].is_a? Hash
-             args[1]
-           end
-         elsif args.is_a? Hash
-           args
-         end
-       end
-       options ||= {}
+       options = args.extract_options!
        if (range=options.delete(:when))
-         options[:when_field] ||= :created_at
-         when_field=options.delete(:when_field)
+         when_field=options.delete(:when_field) || :created_at
          with_dated_scope(when_field, range) { count_without_when(*args) }
        else
          count_without_when(*args)
@@ -26,22 +14,10 @@ module Dreamer3 # :nodoc:
      end
    
      def find_with_when(*args)
-       options=begin
-         if args.is_a? Array
-           if args[0].is_a? Hash
-             args[0]
-           elsif args[1].is_a? Hash
-             args[1]
-           end
-         elsif args.is_a? Hash
-           args
-         end
-       end
-       options ||= {}
+       options = args.extract_options!
        if (range=options[:when])
          options.delete(:when)
-         options[:when_field] ||= :created_at
-         when_field=options.delete(:when_field)
+         when_field=options.delete(:when_field) || :created_at
          with_dated_scope(when_field, range) { find_without_when(*args) }
        else
          find_without_when(*args)
@@ -53,16 +29,29 @@ module Dreamer3 # :nodoc:
      # with_dated_scope :books, :created_at, :this_year
      # with_dated_scope :books, :updated_at, :this_month
      #
-     def with_dated_scope(date_field, time_range)
-       if date_field.to_s.include? "."
-         field = date_field
-       else
-         field = "#{table_name}.#{date_field}"
-       end
+     def with_dated_scope(field, time_range)
+       field = "#{table_name}.#{field}" unless field.to_s.include? "."
        with_scope(:find => {:conditions => date_range_conditions(field, time_range) } ) do 
          yield
        end
      end
+
+     private
+
+     DATE_RANGES={
+       # daily
+       :yesterday => [ Time.zone.now.yesterday.midnight, 1.day ],
+       :today => [ Time.zone.now.midnight, 1.day ],
+       :tomorrow => [ Time.zone.now.tomorrow.midnight, 1.day ],
+       # yearly
+       :last_year => [ Time.zone.now.last_year.beginning_of_year, 1.year ],
+       :this_year => [ Time.zone.now.beginning_of_year, 1.year ],
+       :next_year => [ Time.zone.now.next_year.beginning_of_year, 1.year ],
+       # monthly
+       :last_month => [ Time.zone.now.last_month.beginning_of_month, 1.month ],
+       :this_month => [ Time.zone.now.beginning_of_month, 1.month ],
+       :next_month => [ Time.zone.now.next_month.beginning_of_month, 1.month ],
+     }
 
      # returns a conditions array to scope a specific field to a certain time range
      def date_range_conditions(field, time_range)
@@ -70,26 +59,11 @@ module Dreamer3 # :nodoc:
        if time_range.is_a?(Range)
          conditions << time_range.begin
          conditions << time_range.end
+       elsif range=DATE_RANGES[time_range]
+         conditions << range.first
+         conditions << range.last.is_a?(Fixnum) ? range.first + range.last : range.last
        else
          case time_range
-         when :today
-           conditions << Time.zone.now.midnight
-           conditions << Time.zone.now.tomorrow.midnight
-         when :yesterday
-           conditions << Time.zone.now.yesterday.midnight
-           conditions << Time.zone.now.midnight
-         when :last_year
-           conditions << Time.zone.now.last_year.beginning_of_year
-           conditions << Time.zone.now.beginning_of_year
-         when :this_year
-           conditions << Time.zone.now.beginning_of_year
-           conditions << Time.zone.now.next_year.beginning_of_year
-         when :this_month
-           conditions << Time.zone.now.beginning_of_month
-           conditions << Time.zone.now.next_month.beginning_of_month
-         when :last_month
-           conditions << Time.zone.now.last_month.beginning_of_month
-           conditions << Time.zone.now.beginning_of_month
          # calcuates the count of last month, but only as far as we are in the current month
          # so if we're half way thru April, then this will only show the stats as of half way thru March
          when :last_month_to_date
@@ -100,6 +74,8 @@ module Dreamer3 # :nodoc:
 
            conditions << Time.zone.now.last_month.beginning_of_month
            conditions << end_date
+         else
+           raise "invalid date range provided"
          end
        end
        conditions
