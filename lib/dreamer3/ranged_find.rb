5fc4ac4b10e1a11abc2609ac50316fe5e596bd7e
Josh Goebel <dreamer3@gmail.com>
2011-12-23 06:59:52 -0500

    update README, upgrade for rails 3+

--- lib/dreamer3/ranged_find.rb
+++ lib/dreamer3/ranged_find.rb
@@ -1,58 +1,31 @@
 module Dreamer3 # :nodoc:
   module RangedFind # :nodoc:
+    
+    DATE_RANGES={
+          # daily
+          :yesterday => [ Time.zone.now.yesterday.midnight, 1.day ],
+          :today => [ Time.zone.now.midnight, 1.day ],
+          :tomorrow => [ Time.zone.now.tomorrow.midnight, 1.day ],
+          # yearly
+          :last_year => [ Time.zone.now.prev_year.beginning_of_year, 1.year ],
+          :this_year => [ Time.zone.now.beginning_of_year, 1.year ],
+          :next_year => [ Time.zone.now.next_year.beginning_of_year, 1.year ],
+          # monthly
+          :last_month => [ Time.zone.now.prev_month.beginning_of_month, 1.month ],
+          :this_month => [ Time.zone.now.beginning_of_month, 1.month ],
+          :next_month => [ Time.zone.now.next_month.beginning_of_month, 1.month ],
+        }
    
    module ClassMethods
 
-     def count_with_when(*args)
-       options = args.extract_options!
-       if (range=options.delete(:when))
-         when_field=options.delete(:when_field) || :created_at
-         with_dated_scope(when_field, range) { count_without_when(*args) }
-       else
-         count_without_when(*args)
-       end
+     def when(range, field = "created_at")
+       return self if range.nil?
+       field = "#{table_name}.#{field}" unless field.to_s.include?(".")
+       self.where(date_range_conditions(field, range))
      end
-   
-     def find_with_when(*args)
-       options = args.extract_options!
-       if (range=options[:when])
-         options.delete(:when)
-         when_field=options.delete(:when_field) || :created_at
-         with_dated_scope(when_field, range) { find_without_when(*args) }
-       else
-         find_without_when(*args)
-       end
-     end
-
-     # scopes a class to a given time range on a specific date field
-     #
-     # with_dated_scope :books, :created_at, :this_year
-     # with_dated_scope :books, :updated_at, :this_month
-     #
-     def with_dated_scope(field, time_range)
-       field = "#{table_name}.#{field}" unless field.to_s.include? "."
-       with_scope(:find => {:conditions => date_range_conditions(field, time_range) } ) do 
-         yield
-       end
-     end
-
+          
      private
-
-     DATE_RANGES={
-       # daily
-       :yesterday => [ Time.zone.now.yesterday.midnight, 1.day ],
-       :today => [ Time.zone.now.midnight, 1.day ],
-       :tomorrow => [ Time.zone.now.tomorrow.midnight, 1.day ],
-       # yearly
-       :last_year => [ Time.zone.now.last_year.beginning_of_year, 1.year ],
-       :this_year => [ Time.zone.now.beginning_of_year, 1.year ],
-       :next_year => [ Time.zone.now.next_year.beginning_of_year, 1.year ],
-       # monthly
-       :last_month => [ Time.zone.now.last_month.beginning_of_month, 1.month ],
-       :this_month => [ Time.zone.now.beginning_of_month, 1.month ],
-       :next_month => [ Time.zone.now.next_month.beginning_of_month, 1.month ],
-     }
-
+     
      # returns a conditions array to scope a specific field to a certain time range
      def date_range_conditions(field, time_range)
        conditions = ["? <= #{field} and #{field} < ?"]
@@ -61,7 +34,7 @@ module Dreamer3 # :nodoc:
          conditions << time_range.end
        elsif range=DATE_RANGES[time_range]
          conditions << range.first
-         conditions << range.last.is_a?(Fixnum) ? range.first + range.last : range.last
+         conditions << (range.last===Time ? range.last : range.first + range.last)
        else
          case time_range
          # calcuates the count of last month, but only as far as we are in the current month
@@ -70,9 +43,9 @@ module Dreamer3 # :nodoc:
            end_of_this_month=(Time.zone.now.next_month.beginning_of_month-1.day).mday
            end_of_last_month=(Time.zone.now.beginning_of_month-1.day).mday
            ratio=Time.zone.now.mday.to_f/end_of_this_month
-           end_date=Time.zone.now.last_month.beginning_of_month.midnight+(ratio*end_of_last_month).days
+           end_date=Time.zone.now.prev_month.beginning_of_month.midnight+(ratio*end_of_last_month).days
 
-           conditions << Time.zone.now.last_month.beginning_of_month
+           conditions << Time.zone.now.prev_month.beginning_of_month
            conditions << end_date
          else
            raise "invalid date range provided"
